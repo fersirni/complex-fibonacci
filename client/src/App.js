fb1f311213690fb9775909d13ff53415f7c49af0
Fernando Sirni <fsirni@gmail.com>
2024-04-26 02:15:41 -0300

    Replaced Client folder due to issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import './Fib.js'
-import './OtherPage.js'
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
 
 function App() {
   return (
-      <Router>
+    <Router>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
